83d0f8bdbf5c7197c1894885822d3784d4e5461a
Daniel Rozen <drozen@users.noreply.github.com>
2018-07-16 11:35:18 -0400

    Deleted project files

--- Lecture Files/10MinsToPandas.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'Daniel Rozen'
-
-import pandas as pd #rename to pd for easy future reference
-import numpy as np
-import matplotlib.pyplot as plt
--- Lecture Files/Practice.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Daniel Rozen'
-import numpy as np
-A = np.ones((3,3))
-w = np.array([0.0, 0.1, 0.2])
-print A*w
-print (A*w).sum()
-print (A*w).sum(axis=0)
-print (A*w).sum(axis=1)
